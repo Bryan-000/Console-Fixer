6c7b0e737cdd3940d51787b2cb63d676c07e592f
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-01-31 15:12:42 +0000

    cache bepredirects

--- src/BepRedirector.cs
+++ src/BepRedirector.cs
@@ -6,18 +6,35 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 
-/// <summary> Listens to any BepInEx log and redirects them to PLog. </summary>
+/// <summary> Listens to any BepInEx logs and redirects them to PLog. </summary>
 public class BepRedirector : ILogListener
 {
+    /// <summary> Cached loggers. </summary>
+    public Dictionary<string, plog.Logger> Loggers = [];
+
     /// <summary> this method is invoked when ever a log is well uh logged so redirect it pls ty :3 </summary>
     public void LogEvent(object sender, LogEventArgs eventArgs)
     {
         // if it uses some schizo log type or if its unity (which is already handled by the game), dont redirect it
         if (eventArgs.Source.SourceName == "Unity Log" || !bepToPlog.TryGetValue(eventArgs.Level, out Level plogLvl))
             return;
-        
-        new plog.Logger(eventArgs.Source.SourceName)
-            .Record(eventArgs.Data.ToString(), plogLvl, stackTrace: StackTraceUtility.ExtractFormattedStackTrace(new StackTrace(5, true)));
+
+        Record(eventArgs.Source.SourceName, eventArgs.Data.ToString(), plogLvl);
+    }
+
+    /// <summary> Records a log, using the cached loggers. </summary>
+    public void Record(string name, string message, Level logLvl)
+    {
+        plog.Logger Logger;
+        if (Loggers.TryGetValue(name, out var tryResult))
+            Logger = tryResult;
+        else
+        {
+            Logger = new(name);
+            Loggers.Add(name, Logger);
+        }
+
+        Logger.Record(message, logLvl, stackTrace: StackTraceUtility.ExtractFormattedStackTrace(new StackTrace(5, true)));
     }
 
     /// <summary> IDisposable is forcing me to implement this so :P </summary>
