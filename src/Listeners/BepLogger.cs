1d7b8da6b42a52b6f9f380b24f67d67f37e8c09e
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-02-15 00:11:11 +0000

    filtered bep logs

--- src/Listeners/BepLogger.cs
+++ src/Listeners/BepLogger.cs
@@ -4,6 +4,7 @@ using BepInEx.Logging;
 using plog.Models;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using UnityEngine;
 
 /// <summary> Listens to any BepInEx logs and also logs them to PLog. </summary>
@@ -15,8 +16,8 @@ public class BepLogger : ILogListener
     /// <summary> this method is invoked when ever a log is well uh logged so redirect it pls ty :3 </summary>
     public void LogEvent(object sender, LogEventArgs eventArgs)
     {
-        // if it uses some schizo log type or if its unity (which is already handled by the game), dont redirect it
-        if (eventArgs.Source.SourceName == "Unity Log" || !bepToPlog.TryGetValue(eventArgs.Level, out Level plogLvl))
+        // if its filtered mod or it uses some schizo log type, dont redirect it
+        if (Plugin.FilterBepInEx.Value.Split(", ").Contains(eventArgs.Source.SourceName) || !bepToPlog.TryGetValue(eventArgs.Level, out Level plogLvl))
             return;
 
         Record(eventArgs.Source.SourceName, eventArgs.Data.ToString(), plogLvl);
@@ -25,10 +26,7 @@ public class BepLogger : ILogListener
     /// <summary> Records a log, using the cached loggers. </summary>
     public void Record(string name, string message, Level logLvl)
     {
-        plog.Logger Logger;
-        if (Loggers.TryGetValue(name, out var tryResult))
-            Logger = tryResult;
-        else
+        if (!Loggers.TryGetValue(name, out plog.Logger Logger))
         {
             Logger = new(name);
             Loggers.Add(name, Logger);
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,6 +1,7 @@
 ﻿namespace ConsoleFixer;
 
 using BepInEx;
+using BepInEx.Configuration;
 using ConsoleFixer.Listeners;
 using HarmonyLib;
 using System.IO;
@@ -8,34 +9,41 @@ using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
+using BepInExLogger = BepInEx.Logging.Logger;
 
 /// <summary> General Plugin class used for adding the fixed pdb's whenever you don't have them. </summary>
 [BepInPlugin("Bryan_-000-.ConsoleFixer", "ConsoleFixer", "1.5.0")]
 public class Plugin : BaseUnityPlugin
 {
     /// <summary> "It's called harmony because it harms your mental health." - Doomah 24/12/2025 </summary>
-    public Harmony harmony;
+    public Harmony harmony = new Harmony("Bryan_-000-.ConsoleFixer");
+
+    /// <summary> Mods to filter from BepInEx->PLog redirection. </summary>
+    public static ConfigEntry<string> FilterBepInEx;
 
     /// <summary> Loads all the patches and stuff. </summary>
     public void Awake()
     {
         AddFixedPdbs();
-        harmony = new Harmony("Bryan_-000-.ConsoleFixer");
-        harmony.PatchAll(typeof(Patches));
-
         LoadBinds();
+
+        harmony.PatchAll(typeof(Patches));
     }
 
     /// <summary> Handles/Loads all the binds and settings for the mod. </summary>
     public void LoadBinds()
     {
-        bool redirectBep = Config.Bind("Settings", "Log Bep to PLog", true, "Whether to log BepInEx logs to PLog/F8 aswell.").Value;
-        if (redirectBep) 
-            BepInEx.Logging.Logger.Listeners.Add(new BepLogger());
-        
-        bool redirectUnity = Config.Bind("Settings", "Debug build", true, "Whether we should confuse the game into believeing this is a debug build.").Value;
-        if (redirectUnity)
+        // debug build setting
+        if (Config.Bind("Settings", "Debug build", true, "Whether we should confuse the game into believeing this is a debug build. (requires reload)").Value)
             harmony.PatchAll(typeof(DebugBuildPatch));
+
+        // Bep filter setting
+        FilterBepInEx = Config.Bind("Settings", "BepPLogFilter", "Unity Log, UltraEditor", "Mods to filter out from the BepInEx to PLog redirector. (Split by ',')");
+        Logger.LogInfo(string.Join("|", FilterBepInEx.Value.Split(", ")));
+
+        // redirecting bep logs to plog setting
+        if (Config.Bind("Settings", "Log Bep to PLog", true, "Whether to log BepInEx logs to PLog/F8 aswell. (requires reload)").Value)
+            BepInExLogger.Listeners.Add(new BepLogger());
     }
 
     /// <summary> Deletes all the old .pd_ files and adds the new .pdb files. </summary>
