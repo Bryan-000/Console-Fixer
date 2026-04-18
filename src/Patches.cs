ee1fdeb2ffe910c8e85853357f776fde1b88655a
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-04-18 12:18:11 +0100

    make you better

--- src/Patches.cs
+++ src/Patches.cs
@@ -23,19 +23,16 @@ public class Patches
     {
         MethodInfo ReplaceString = AccessTools.Method(typeof(string), "Replace", [typeof(string), typeof(string)]);
 
-        List<CodeInstruction> CompletedInstructions = [];
-
         foreach (CodeInstruction instruction in instructions)
         {
             if (instruction.Calls(ReplaceString))
             {
-                CompletedInstructions.RemoveAt(CompletedInstructions.Count - 1);
-                CompletedInstructions.RemoveAt(CompletedInstructions.Count - 1); // i feel like i could make this better but every other way felt worse or had 2 pops after stuff
+                yield return new(OpCodes.Pop);
+                yield return new(OpCodes.Pop);
             }
-            else CompletedInstructions.Add(instruction);
+            else 
+                yield return instruction;
         }
-
-        return CompletedInstructions;
     }
 
     /// <summary> Basically in Unity StackTrace's replace like `NewMovement` with `Assembly-CSharp.NewMovement`. </summary>
